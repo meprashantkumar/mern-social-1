74577c9ed12c0bc8fbdb681d47effeeba7aa84ba
Ayush-Singh77 <ayush4singh545@gmail.com>
2024-07-31 07:46:39 +0530

    first commit

---
+++ frontend/src/components/chat/Message.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Message = ({ ownMessage, message }) => {
+  return (
+    <div className={`mb-2 ${ownMessage ? "text-right" : "text-left"}`}>
+      <span
+        className={`inline-block p-2 rounded-lg ${
+          ownMessage ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
+        }`}
+      >
+        {message}
+      </span>
+    </div>
+  );
+};
+
+export default Message;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import AddPost from "../components/AddPost";
+import PostCard from "../components/PostCard";
+import { PostData } from "../context/PostContext";
+import { Loading } from "../components/Loading";
+
+const Home = () => {
+  const { posts, loading } = PostData();
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <AddPost type="post" />
+          {posts && posts.length > 0 ? (
+            posts.map((e) => <PostCard value={e} key={e._id} type={"post"} />)
+          ) : (
+            <p>No Post Yet</p>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
