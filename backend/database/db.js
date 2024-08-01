9fafbdf51348512f7b774ea0dc65ae9c154ff727
Ayush-Singh77 <113441580+Ayush-Singh77@users.noreply.github.com>
2024-08-01 15:08:13 +0530

    Update db.js CHANGED DB NAME

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
-      dbName: "YoutubeMernSocial",
+      dbName: "MernSocial",
     });
 
     console.log("Connected To MongoDb");
