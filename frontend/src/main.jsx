6f8a0b96d0199a4910a8e0e7d716c57dd6d214df
Prashant kumar Chaturvedi <93850004+meprashantkumar@users.noreply.github.com>
2024-08-01 15:21:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { UserContextProvider } from "./context/UserContext.jsx";
+import { PostContextProvider } from "./context/PostContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
+import { ChatContextProvider } from "./context/ChatContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UserContextProvider>
+      <PostContextProvider>
+        <ChatContextProvider>
+          <SocketContextProvider>
+            <App />
+          </SocketContextProvider>
+        </ChatContextProvider>
+      </PostContextProvider>
+    </UserContextProvider>
+  </React.StrictMode>
+);
+
